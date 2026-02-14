99460282b49248b44358fd9b282013cd261d2b17
s3fyi8u <ahmed,gabir.9.5@gmail.com>
2026-02-14 08:06:43 +0300

    Update API URL to api.sdotist.org

--- flutter_app/lib/core/constants/api_constants.dart
+++ flutter_app/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "https://sdotist.org"; // Production Server
+  static const String baseUrl = "https://api.sdotist.org"; // Production Server
   // static const String baseUrl = "http://127.0.0.1:8000"; // For iOS Simulator
   // static const String baseUrl = "http://10.0.2.2:8000"; // For Android Emulator
   
